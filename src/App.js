6e9c18157297590ec04291bef8888e16ddb70893
Pav <pav0107@gmail.com>
2022-10-31 20:40:55 +0000

    finish react bits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { AddTransaction } from './AddTransaction';
 import './App.css';
+import { AddTransaction } from './components/AddTransaction';
 import { Balance } from './components/Balance';
 import { Header } from './components/Header';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 
+import { GlobalProvider } from './context/GlobalState';
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -15,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
